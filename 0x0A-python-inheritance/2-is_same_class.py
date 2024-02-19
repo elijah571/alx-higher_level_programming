07726a07423b385980379b026a0119b8807c1241
elijah571 <elijahfx43@gmail.com>
2024-02-19 08:23:24 -0800

    0x0A-python-inheritance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+================================
+module with method is_same_class
+================================
+"""
+
+
+def is_same_class(obj, a_class):
+    return type(obj) is a_class
